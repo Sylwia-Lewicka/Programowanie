18dde87fa9a466a52b4f050374335a6a57890003
Sylwia-Lewicka <lewicka9@gmail.com>
2020-11-30 16:00:17 +0100

    Add new files

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "tstring.h"
+using namespace std;
+
+auto fun(const char* c) {
+    return TString(c);
+}
+
+auto main() -> int {
+    TString s1;
+    TString s2{"abrakadabra"};
+    TString s3 { s2 };
+    TString s4 = fun("nowy obiekt nr 1");
+    s1 = s2;
+    s1 = std::move( s2 );
+    s1.insert(0,"NA POCZATEK");
+    s1.insert(s1.size(),'K');
+    s1.insert(s1.size()/2,"W SRODKU");
+    for (auto i=0ul; i<s1.size(); ++i) cout << s1[i];
+    cout << endl;
+    for (const auto& c : s1) {
+        //c = toupper(c);
+        cout << c << "-";
+    }
+    cout << endl;
+    for (auto i=0ul; i<s1.size(); ++i) cout << s1[i];
+    cout << endl;
+    cout << "Wpisz jakies dane: ";
+    cin >> s1;
+    s1.push_back("-koncowka-");
+    s1.push_back('X');
+    cout << "Wpisano: " << s1 << endl;
+    cout << "----------------------" << endl;
+}
---
+++ tstring.h
@@ -0,0 +1,48 @@
+#ifndef TSTRING_H
+#define TSTRING_H
+
+#include <cstring>
+#include <iostream>
+
+class TString {
+
+    public:
+        TString(const char* s = nullptr); // c-tor
+        TString( const TString& s ); // cc-tor
+        TString( TString&& s ); // move c-tor
+        TString& operator=( const TString& s ); // copy operator=
+        TString& operator=( TString&& s ); // move operator=
+        ~TString(); // d-tor
+
+        char& operator[]( size_t n );
+        const char& operator[]( size_t n ) const;
+
+        char* begin() { return ptr; }
+        char* end() { return ptr+len; }
+        size_t length() const { return len; } 
+        size_t size() const { return len; } 
+        void clear() { delete [] ptr; ptr = nullptr; len = 0; }
+        bool empty() const { return len ? false : true; }
+        char& front() { return *ptr; }
+        const char& front() const { return *ptr; }
+        char& back() { return *(ptr+len-1); }
+        const char& back() const { return *(ptr+len-1); }
+        char* c_str() { return ptr; }
+        const char* c_str() const { return ptr; }
+        void push_back( const char* c ) { insert( len, c ); }
+        void push_back( char c ) { insert( len, c ); }
+
+        char* insert( size_t pos, const char* c );
+        char* insert( size_t pos, char c );
+        char* erase( size_t pos = 0, size_t len = 0 );
+
+        friend std::ostream& operator<<( std::ostream& strumien, const TString& s );
+        friend std::istream& operator>>( std::istream& strumien, TString& s );
+
+    private:
+        char* ptr { nullptr };
+        size_t len { 0 };
+
+};
+
+#endif // TSTRING_H
